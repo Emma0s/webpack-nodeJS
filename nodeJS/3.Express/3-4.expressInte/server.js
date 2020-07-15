d418269ed583b749cc178e55f3f71c2a8eb02335
emma0s <joy0heaven@sina.cn>
2020-07-15 12:00:02 +0800

    add consolidate test

--- nodeJS/3.Express/3-4.expressInte/server.js
+++ nodeJS/3.Express/3-4.expressInte/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const ejs = require('ejs');
 const jade = require('jade');
+const consolidate = require('consolidate');
 
 let server = express();
 server.listen(8080);
@@ -20,10 +21,20 @@ for(let i=0;i<10000;i++){
 server.use(cookieSession({name: 'sww_id',keys: arr, maxAge: 20*3600*1000}));
 //3.post数据
 server.use(bodyParser.urlencoded({extended: false}));
-server.use(multer({dest: './www/upload'}).any())
+server.use(multer({dest: './www/upload'}).any());
+
+//4.配置模板引擎
+//输出什么东西
+server.set('view engine','html');
+//模板文件放在哪儿
+server.set('views','./views');
+//用哪种模板引擎
+server.engine('html',consolidate.ejs);
+server.get('/index',(req,res)=>{
+    res.render('1.ejs',{name: 'blue'});
+})
+
 //用户请求
-server.use('/',(req,res,next)=>{
-    console.log(req.query,req.body,req.files,req.cookies,req.session);
-});
+// 
 //4.static数据
 server.use(static('./www'));
